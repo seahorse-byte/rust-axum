73c6c862e2c4908374deb3e3d1fc2d8ee8053b0d
Olsi Gjeci <olsi.developer@gmail.com>
2023-09-16 14:35:01 +0100

    be, fe, test, add serde

---
+++ README.md
@@ -0,0 +1,9 @@
+#useful commands
+
+(BE-server)
+`cargo watch -q -c -w src/ -x run` 
+
+
+
+(FE - tests)
+`cargo watch -q -c -w tests/ -x "test -q quick_dev -- --nocapture"` 
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,27 @@
-
 #![allow(unused)]
+
+use std::net::SocketAddr;
+
+use axum::{
+    http::StatusCode,
+    response::{Html, IntoResponse},
+    routing::get,
+    Json, Router,
+};
 
 #[tokio::main]
 async fn main() {
-    println!("Hello, world!");
+    let app = Router::new().route("/hello", get(handler_hello));
+
+    async fn handler_hello() -> impl IntoResponse {
+        // println!("->> {:<12} - handler_hello", "HANDLER");
+        Html("<h1>Hello, World!</h1>")
+    }
+
+    let addr = SocketAddr::from(([127, 0, 0, 1], 8080));
+    println!("Listening on http://{}", addr);
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
 }
