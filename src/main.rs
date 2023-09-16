3a02927756b06111d18bbf91a8f1285c09a91a32
Olsi Gjeci <olsi.developer@gmail.com>
2023-09-16 15:17:01 +0100

    pass params and read using Query from axum

--- src/main.rs
+++ src/main.rs
@@ -3,19 +3,27 @@
 use std::net::SocketAddr;
 
 use axum::{
+    extract::Query,
     http::StatusCode,
     response::{Html, IntoResponse},
     routing::get,
     Json, Router,
 };
+use serde::{Deserialize, Serialize};
 
 #[tokio::main]
 async fn main() {
     let app = Router::new().route("/hello", get(handler_hello));
 
-    async fn handler_hello() -> impl IntoResponse {
-        // println!("->> {:<12} - handler_hello", "HANDLER");
-        Html("<h1>Hello, World!</h1>")
+    #[derive(Debug, Deserialize)]
+    struct HelloParams {
+        name: Option<String>,
+    }
+
+    async fn handler_hello(Query(params): Query<HelloParams>) -> impl IntoResponse {
+        println!("->> {:<12} - handler_hello - {params:?}", "HANDLER");
+        let name = params.name.as_deref().unwrap_or("World");
+        Html(format!("<h1>Helooo {name}</h1>"))
     }
 
     let addr = SocketAddr::from(([127, 0, 0, 1], 8080));
--- tests/quick_dev.rs
+++ tests/quick_dev.rs
@@ -5,7 +5,7 @@ use anyhow::Result;
 #[tokio::test]
 async fn quick_dev() -> Result<()> {
     let hc = httpc_test::new_client("http://localhost:8080")?;
-    hc.do_get("/hello").await?.print().await?;
+    hc.do_get("/hello?name=olsi").await?.print().await?;
 
     Ok(())
 }
