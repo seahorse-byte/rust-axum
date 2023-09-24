65f102d808e712c88a46ac73bd0034b9b5924f4c
Olsi Gjeci <olsi.developer@gmail.com>
2023-09-24 18:29:34 +0100

    use ctx in models as well

--- src/model.rs
+++ src/model.rs
@@ -1,7 +1,10 @@
 // simplistic model
 // with mock store layer
 
-use crate::error::{Error, Result};
+use crate::{
+    ctx::Ctx,
+    error::{Error, Result},
+};
 use serde::{de, Deserialize, Serialize};
 use std::sync::{Arc, Mutex};
 
@@ -9,6 +12,7 @@ use std::sync::{Arc, Mutex};
 #[derive(Debug, Clone, Serialize)]
 pub struct Ticket {
     pub id: u64,
+    pub cid: u64, // creator user_id
     pub title: String,
 }
 #[derive(Deserialize)]
@@ -36,18 +40,19 @@ impl ModelController {
 // CRUD
 
 impl ModelController {
-    pub async fn create_ticket(&self, ticket_fc: TicketForCreate) -> Result<Ticket> {
+    pub async fn create_ticket(&self, ctx: Ctx, ticket_fc: TicketForCreate) -> Result<Ticket> {
         let mut store = self.tickers_store.lock().unwrap();
         let id = store.len() as u64;
         let ticket = Ticket {
             id,
+            cid: ctx.user_id(),
             title: ticket_fc.title,
         };
         store.push(Some(ticket.clone()));
         Ok(ticket)
     }
 
-    pub async fn list_tickets(&self) -> Result<Vec<Ticket>> {
+    pub async fn list_tickets(&self, _ctx: Ctx) -> Result<Vec<Ticket>> {
         let store = self.tickers_store.lock().unwrap();
         let tickets = store
             .iter()
@@ -56,7 +61,7 @@ impl ModelController {
         Ok(tickets)
     }
 
-    pub async fn delete_ticket(&self, id: u64) -> Result<Ticket> {
+    pub async fn delete_ticket(&self, _ctx: Ctx, id: u64) -> Result<Ticket> {
         let mut store = self.tickers_store.lock().unwrap();
         let ticket = store.get_mut(id as usize).and_then(|ticket| ticket.take());
         ticket.ok_or(Error::TicketDeleteFailIDNotFound { id })
